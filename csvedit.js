dfa70ceac5884a10ce936843473ff1854cc39471
Ian Fisher <iafisher@protonmail.com>
2020-05-08 16:15:44 -0700

    Make it look more like a spreadsheet

--- csvedit.js
+++ csvedit.js
@@ -1,38 +1,39 @@
 'use strict';
-const electron = require('electron');
 
-let activeInput = null;
+const electron = require('electron');
 
 function populateTable(rows) {
   const thead = document.querySelector('thead');
   const tbody = document.querySelector('tbody');
 
-  const columnWidths = getColumnWidths(rows);
-  const header = document.createElement('tr');
-  for (let i = 0; i < rows[0].length; i++) {
-    const th = document.createElement('th');
-    th.textContent = rows[0][i];
-    th.style.width = columnWidths[i] + "px";
-    header.appendChild(th);
-  }
-  thead.appendChild(header);
+  const nrows = Math.max(rows.length, 100);
+  const ncols = Math.max(rows[0].length, 26);
 
-  for (const row of rows.slice(1)) {
+  for (let i = 0; i < nrows; i++) {
     const tr = document.createElement('tr');
-    for (let i = 0; i < row.length; i++) {
-      const td = document.createElement('td');
-      td.textContent = row[i];
-      td.style.width = columnWidths[i] + "px";
-      td.addEventListener('click', onClick);
+    for (let j = 0; j < ncols; j++) {
+      const td = document.createElement(i === 0 ? 'th' : 'td');
+      if (i < rows.length && j < rows[i].length) {
+        td.textContent = rows[i][j];
+      }
       tr.appendChild(td);
     }
-    tbody.appendChild(tr);
+
+    if (i === 0) {
+      thead.appendChild(tr);
+    } else {
+      tbody.appendChild(tr);
+    }
   }
 
-  const win = electron.remote.getCurrentWindow();
-  const table = document.querySelector('table');
+  document.body.addEventListener('click', onClick);
+
+  // const win = electron.remote.getCurrentWindow();
+  // const table = document.querySelector('table');
   // win.setContentSize(table.offsetWidth, table.offsetHeight);
 }
+
+let activeInput = null;
 
 function onClick(event) {
   if (activeInput !== null) {
@@ -44,8 +45,11 @@ function onClick(event) {
   }
 
   const td = event.target;
+  if (td.tagName !== 'TD' && td.tagName != 'TH') {
+    return;
+  }
   const text = td.textContent;
-  td.innerHTML = "";
+  td.innerHTML = '';
   const input = document.createElement('input');
   input.value = text;
   input.addEventListener('keyup', onKeyup);
@@ -65,28 +69,6 @@ function submitInput(input) {
   td.innerHTML = "";
   td.textContent = text;
   activeInput = null;
-}
-
-function getColumnWidths(rows) {
-  const columnWidths = [];
-  for (let i = 0; i < rows[0].length; i++) {
-    const width = getMaxWidth(rows, i);
-    columnWidths.push(width);
-  }
-  return columnWidths;
-}
-
-function getMaxWidth(rows, colno) {
-  let maxWidth = 0;
-  for (const row of rows) {
-    maxWidth = Math.max(getTextWidth(row[colno]), maxWidth);
-  }
-  return maxWidth;
-}
-
-function getTextWidth(text) {
-  // This only works with a fixed-width font.
-  return text.length * 8.5 + /* padding */ 15;
 }
 
 const rows = electron.remote.getGlobal('sharedObject').rows;
--- main.js
+++ main.js
@@ -5,37 +5,47 @@ const Papa = require('papaparse');
 
 function createWindow () {
   fs.readFile(process.argv[2], { encoding: 'utf-8' }, (err, data) => {
-    if (err) throw err;
+    if (err) {
+      throw err;
+    }
 
     const rows = Papa.parse(data, { skipEmptyLines: true }).data;
     const mainWindow = new BrowserWindow({
-      width: 1500,
-      height: 400,
+      allowRendererProcessReuse: false,
+      show: false,
       webPreferences: {
         nodeIntegration: true,
       }
-    })
+    });
+
+    mainWindow.maximize();
+    mainWindow.show();
 
     global.sharedObject = {
       path: process.argv[2],
       rows: rows,
+      bounds: mainWindow.getContentBounds(),
     };
 
-    mainWindow.loadFile('index.html')
-    mainWindow.webContents.openDevTools()
+    mainWindow.loadFile('index.html');
+    mainWindow.webContents.openDevTools();
   });
 }
 
-app.whenReady().then(createWindow)
+app.whenReady().then(createWindow);
 
 app.on('window-all-closed', function () {
   // On macOS it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') app.quit()
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
 })
 
 app.on('activate', function () {
   // On macOS it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
-  if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
 })
